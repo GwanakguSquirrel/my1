9460b593b0352a8b18eb815eaf25b39ff1545999
test <test@test.com>
2020-01-05 15:28:13 +0900

    e

--- gitpush.py
+++ gitpush.py
@@ -1,5 +1,6 @@
 #%%
 import os
+import subprocess
 
 def git_config(str_directory_path, name, email):
     os.chdir(str_directory_path)
@@ -14,6 +15,11 @@ def git_push(str_directory_path, id, password):
     os.system("git add .")
     os.system("git commit -m 'e'")
     git_config(str_directory_path, name, email)
-    os.system(
-        "git push https://" + id +":" + password + "@github.com/GwanakguSquirrel/my1.git --all"
-        )
+
+    cmd = ["git push https://" + id +":" + password + "@github.com/GwanakguSquirrel/my1.git --all"]
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    o, e = proc.communicate()
+    o = o.decode('ascii')
+    e = e.decode('ascii')
+    print("o:", o)
+    print("e:", e)
