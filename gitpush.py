5be3786acea56edacdae35ee5efa267f01dad5a2
test <test@test.com>
2020-01-05 15:22:11 +0900

    e

---
+++ gitpush.py
@@ -0,0 +1,19 @@
+#%%
+import os
+
+def git_config(str_directory_path, name, email):
+    os.chdir(str_directory_path)
+    os.system("git config user.name " + name)
+    os.system("git config user.email " + email)
+
+def git_push(str_directory_path, id, password):
+    name = "test"
+    email = "test@test.com"
+    os.chdir(str_directory_path)
+
+    os.system("git add .")
+    os.system("git commit -m 'e'")
+    git_config(str_directory_path, name, email)
+    os.system(
+        "git push https://" + id +":" + password + "@GwanakguSquirrel/my1.git --all"
+        )
